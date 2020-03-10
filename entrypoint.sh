aba82c78a747b50475ad225db504779938b7955f
lakilla21 <lakilla21@gmail.com>
2020-03-10 00:09:55 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,15 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Load Config Files
-#[ ! -d /home/container/ConanSandbox/Saved/Config/WindowsServer ] && mkdir -p /home/container/ConanSandbox/Saved/Config/WindowsServer/
-#[ ! -f /home/container/ConanSandbox/Saved/Config/WindowsServer/Engine.ini ] && wget http://raw.githubusercontent.com/lakilla21/testdocker/master/Engine.ini -P /home/container/ConanSandbox/Saved/Config/WindowsServer/
+[ ! -d /home/container/Configs ] && mkdir -p /home/container/Configs/
+[ ! -f /home/container/Configs/Users.eco ] && wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/Users.eco?token=AFMH75624JG67KBIX4WRLIK6M44VA -P /home/container/Configs/
+[ ! -f /home/container/Configs/Difficulty.eco ] && wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/Difficulty.eco?token=AFMH7564W66QVBOKOFMH7WS6M44ZG -P /home/container/Configs/
+[ ! -f /home/container/Configs/Sleep.eco ] && wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/Sleep.eco?token=AFMH75YBYR77TAMV7KUV2AK6M446Y -P /home/container/Configs/
+
+# Adding Blacklist, Whitelist, and Admins
+sed -i "10s/[]/[${WHITELIST}]/" /home/container/Configs/Users.eco
+sed -i "16s/[]/[${BLACKLIST}]/" /home/container/Configs/Users.eco
+sed -i "22s/[]/[${ADMINS}]/" /home/container/Configs/Users.eco
 
 # Replace Startup Variables
 MODIFIED_STARTUP="eval $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')"
