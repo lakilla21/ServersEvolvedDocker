2f6b9b288fb03ca37bd8bb3608e17d8862786085
lakilla21 <lakilla21@gmail.com>
2020-03-28 19:47:36 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,19 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 rm /home/container/Configs/UsersInput.eco
 rm /home/container/Configs/DifficultyInput.eco
 rm /home/container/Configs/SleepInput.eco
+rm /home/container/Configs/NetworkInput.eco
 
 [ ! -d /home/container/Configs ] && mkdir -p /home/container/Configs/
 wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/UsersInput.eco -P /home/container/Configs/
 wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/DifficultyInput.eco -P /home/container/Configs/
 wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/SleepInput.eco -P /home/container/Configs/
+wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/NetworkInput.eco -P /home/container/Configs/
 
 if [[ ${MANUAL_CONFIG} = 'false' ]]; then
-  envsubst < UsersInput.eco > Users.eco
-  envsubst < DifficultyInput.eco > Difficulty.eco
-  envsubst < SleepInput.eco > Sleep.eco
+  envsubst < /home/container/Configs/UsersInput.eco > Users.eco
+  envsubst < /home/container/Configs/DifficultyInput.eco > Difficulty.eco
+  envsubst < /home/container/Configs/SleepInput.eco > Sleep.eco
+  envsubst < /home/container/Configs/NetworkInput.eco > Network.eco
 fi
 
 # Replace Startup Variables
