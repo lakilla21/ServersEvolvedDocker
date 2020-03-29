8f33e1eb18f409861a71fea52f87aa1252c64b54
lakilla21 <lakilla21@gmail.com>
2020-03-28 22:33:18 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/
 wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/NetworkInput.eco -P /home/container/Configs/
 
 if [[ ${MANUAL_CONFIG} = 'false' ]]; then
-  envsubst < /home/container/Configs/UsersInput.eco > /home/container/Configs/Users.eco
+  envsubst '${WHITELIST},${BLACKLIST},${ADMINS}' < /home/container/Configs/UsersInput.eco > /home/container/Configs/Users.eco
   envsubst < /home/container/Configs/DifficultyInput.eco > /home/container/Configs/Difficulty.eco
   envsubst < /home/container/Configs/SleepInput.eco > /home/container/Configs/Sleep.eco
   envsubst < /home/container/Configs/NetworkInput.eco > /home/container/Configs/Network.eco
