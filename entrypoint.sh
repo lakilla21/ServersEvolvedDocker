8258b86b41fd24981c0cbf0bb02d72e551b5b23f
lakilla21 <lakilla21@gmail.com>
2020-03-28 19:34:44 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,20 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Load Config Files
-[ ! -d /home/container/Configs ] && mkdir -p /home/container/Configs/
-[ ! -f /home/container/Configs/Users.eco ] && wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/Users.eco -P /home/container/Configs/
-[ ! -f /home/container/Configs/Difficulty.eco ] && wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/Difficulty.eco -P /home/container/Configs/
-[ ! -f /home/container/Configs/Sleep.eco ] && wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/Sleep.eco -P /home/container/Configs/
+rm /home/container/Configs/UsersInput.eco
+rm /home/container/Configs/DifficultyInput.eco
+rm /home/container/Configs/SleepInput.eco
 
-# Adding Blacklist, Whitelist, and Admins
-sed -i "10 s/"$values":.*/"$values": [${WHITELIST}]/" /home/container/Configs/Users.eco
-sed -i "16 s/"$values":.*/"$values": [${BLACKLIST}]/" /home/container/Configs/Users.eco
-sed -i "22 s/"$values":.*/"$values": [${ADMINS}]/" /home/container/Configs/Users.eco
+[ ! -d /home/container/Configs ] && mkdir -p /home/container/Configs/
+wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/UsersInput.eco -P /home/container/Configs/
+wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/DifficultyInput.eco -P /home/container/Configs/
+wget http://raw.githubusercontent.com/lakilla21/ServersEvolvedDocker/ECO-Server/SleepInput.eco -P /home/container/Configs/
+
+if [[ ${MANUAL_CONFIG} = 'false' ]]; then
+  envsubst < UsersInput.eco > Users.eco
+  envsubst < DifficultyInput.eco > Difficulty.eco
+  envsubst < SleepInput.eco > Sleep.eco
+fi
 
 # Replace Startup Variables
 MODIFIED_STARTUP="eval $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')"
